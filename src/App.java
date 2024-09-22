462033261742d53f2b0a3dbfb795914b93a852f9
Amiraaa569 <abidoamira@gmail.com>
2024-09-22 19:25:58 +0300

    Creating a method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        List<String> students = readStudentsFromFile("students.txt");
+        for (String student : students) {
+            System.out.println(student);}
+
     }
+    public static List<String> readStudentsFromFile(String filename) {
+        List<String> students = new ArrayList<>();
+        
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                students.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+
+        return students;
+    }
+
+
 }
