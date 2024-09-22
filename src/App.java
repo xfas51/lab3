6f13edc0485877a2c81338ba83a0680745c92528
Amiraaa569 <abidoamira@gmail.com>
2024-09-22 19:45:21 +0300

    creating the second method

--- src/App.java
+++ src/App.java
@@ -3,12 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         List<String> students = readStudentsFromFile("students.txt");
         for (String student : students) {
-            System.out.println(student);}
+            System.out.println(student);
+        }
+        int groupSize = 2; 
+        List<List<String>> groups = createRandomGroups(students, groupSize);
+            
+        for (List<String> group : groups) {
+            System.out.println(groups);
+        }
+    
 
     }
     public static List<String> readStudentsFromFile(String filename) {
@@ -25,6 +34,16 @@ public class App {
 
         return students;
     }
-
+    public static List<List<String>> createRandomGroups(List<String> students, int groupSize) {
+        Collections.shuffle(students); // Shuffle the student list
+        List<List<String>> groups = new ArrayList<>();
+        
+        for (int i = 0; i < students.size(); i += groupSize) {
+            int end = Math.min(i + groupSize, students.size());
+            groups.add(new ArrayList<>(students.subList(i, end)));
+        }
+        
+        return groups;
+    }
 
 }
